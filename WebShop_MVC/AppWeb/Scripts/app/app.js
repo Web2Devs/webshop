8129bfd9ab9a8c859621a67f0e08bd5cc00071d9
carlosx_1302@hotmail.com <carlosx>
2016-06-16 20:44:40 +0000

    WebShop Update Web
    Update

--- WebShop_MVC/AppWeb/Scripts/app/app.js
+++ WebShop_MVC/AppWeb/Scripts/app/app.js
@@ -6,13 +6,16 @@ var appmvc = angular.module('appmvc', ['angular-loading-bar']);
 
 appmvc.controller('HomeController', function ($scope, $http) {
     $scope.productos = [];
+    $scope.loading = true;
     $http.get('/Content/promo.json')
         .success(function (data) {
             $scope.productos = chunkArray(data, 3);
         })
         .error(function (data, status, headers, config) {
             console.log("error");
-        });
+        }).finally(function () {
+            $scope.loading = false;
+        });;
 });
 
 
