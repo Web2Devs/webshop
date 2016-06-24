c09508ab6ded83348a2f7765fea069b05776264f
Carlos Cp <carlosx_1302@hotmail.com>
2016-06-24 16:58:31 -0500

    WebShop Implementacion Servicio Rest
    Servicio REST

---
+++ WebShop_MVC/AppWeb.ServicioRest/IRestService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AppWeb.Framework.Data.DB.Model;
+
+namespace AppWeb.ServicioRest
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IRestService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Clientes", ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<TCliente> ListaClientes();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Productos", ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<TProducto> ListaProductos();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/SubCategoria", ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<TSubCategoria> ListaSubCategorias();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Categorias", ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<TCategoria> ListaCategorias();
+    }
+}
---
+++ WebShop_MVC/AppWeb.ServicioRest/RestService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AppWeb.Framework.Data.DB.Model;
+
+namespace AppWeb.ServicioRest
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "RestService" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione RestService.svc o RestService.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class RestService : IRestService
+    {
+        public IEnumerable<TCategoria> ListaCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TCliente> ListaClientes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TProducto> ListaProductos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TSubCategoria> ListaSubCategorias()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
