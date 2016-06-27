607e9f04f964a64475adc3f2fb340c0f90081e59
jean360 <sorvivor_5@hotmail.com>
2016-06-27 15:16:16 -0500

    WebShop Controller

--- WebShop_MVC/AppWeb.Dashboard/Controllers/ClienteController.cs
+++ WebShop_MVC/AppWeb.Dashboard/Controllers/ClienteController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using AppWeb.Dashboard.ShopWS;
 namespace AppWeb.Dashboard.Controllers
 {
     public class ClienteController : Controller
@@ -11,7 +11,46 @@ namespace AppWeb.Dashboard.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
+            ServiceShopSoaClient proxy = new ServiceShopSoaClient();
+            var listado = proxy.ListaProductos();               
+            return View(listado);
+        }
+        public ActionResult Crear()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Crear(TProducto nuevoProducto)
+        {
+
+            var proxy = new ServiceShopSoaClient();
+            proxy.AgregarProducto(nuevoProducto);
+            return RedirectToAction("Index");
+        }
+        
+        [HttpGet]
+        public ActionResult Actualizar(int? id)
+        {
+            var proxy = new ServiceShopSoaClient();
+            if (id.HasValue)
+            {
+                
+                var prod = proxy(id.Value);
+                return View(prod);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public ActionResult Actualizar(TProducto modificarProducto)
+        {
+            var proxy = new ServiceShopSoaClient();
+            proxy.ActualizarProducto(modificarProducto);
+            return RedirectToAction("Index");
+        }
+
     }
 }
